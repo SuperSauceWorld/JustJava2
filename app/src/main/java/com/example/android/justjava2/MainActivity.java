07f9af7e3c7256c5ef137fef1323a96b7e22f00b
eddyandrade <eddyandrade28@gmail.com>
2017-11-06 10:40:52 -0600

    This is the (final) build of the app, hopefully.

--- app/src/main/java/com/example/android/justjava2/MainActivity.java
+++ app/src/main/java/com/example/android/justjava2/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.android.justjava2;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -25,6 +27,10 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the plus button is clicked.
      */
     public void increment(View view) {
+        if (quantity==100) {
+            return;
+        }
+
         quantity=quantity+1;
         displayQuantity(quantity);
 
@@ -36,6 +42,10 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the minus button is clicked.
      */
     public void decrement(View view) {
+        if (quantity==1) {
+            return;
+        }
+
         quantity=quantity-1;
         displayQuantity(quantity);
 
@@ -54,18 +64,41 @@ public class MainActivity extends AppCompatActivity {
         boolean hasWhippedCream=whippedCreamCheckBox.isChecked();
         boolean hasChocolate=chocolateCheckBox.isChecked();
 
-        int price=calculatePrice();
-        String priceMessage=createOrderSummary(name, price, hasWhippedCream, hasChocolate);
-        displayMessage(priceMessage);
+       int price=calculatePrice(hasWhippedCream, hasChocolate);
+       String priceMessage=createOrderSummary(name, price, hasWhippedCream, hasChocolate);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto")); //Only email app should handle this.
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Just Java order for" + name);
+        intent.putExtra(Intent.EXTRA_TEXT, priceMessage);
+        if (intent.resolveActivity(getPackageManager()) !=null) {
+            startActivity(intent);
+        }
+
+
     }
 
     /**
      * Calculates the price of the order.
-     *
+     * @param addWhippedCream is whether or not the user wants whipped cream topping.
+     *@param addChocolate is whether or not the user wants chocolate topping.
      * @return total price
      *
      */
-    private int calculatePrice() {
+    private int calculatePrice(boolean addWhippedCream, boolean addChocolate) {
+        //Price of one cup of coffee.
+        int basePrice=5;
+
+        //Add $1 if the user wants whipped cream.
+        if (addWhippedCream) {
+           basePrice=basePrice+1;
+        }
+
+        //Add $2 if the user wants chocolate.
+        if (addChocolate) {
+            basePrice=basePrice+2;
+        }
+        //Calculate the total order price by multiplying by quantity.
         return quantity * 5;
     }
 
@@ -90,13 +123,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    /**
-     * This method displays the given text on the screen.
-     */
-    private void displayMessage(String message) {
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(message);
-    }
 
     /**
      * This method displays the given quantity value on the screen.
